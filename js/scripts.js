1085fbb206be3d0334db10bfc333134fff875112
Chris Wingo and Dave Denlow <chris@>
2016-11-02 09:28:26 -0700

    Set up initial files and make the dice object with  a roll prototype.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+function Die() {
+  this.pip = 1;
+}
+
+Die.prototype.roll = function() {
+  this.pip = Math.floor((Math.random() * 6) + 1);
+}
+
+$(document).ready(function(){
+  die1 = new Die();
+  die1.roll();
+  console.log(die1.pip);
+  $("#player1-roll").click(function(){
+    die1.roll();
+    $("#player1").text(die1.pip);
+  })
+});
