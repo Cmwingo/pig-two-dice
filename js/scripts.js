2fd642b50ff40cd4c678c7d8409adf0256ea676f
Chris Wingo and Dave Denlow <dave@>
2016-11-02 09:54:27 -0700

    add player totals, player 2 roll and total

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,38 @@
 function Die() {
   this.pip = 1;
-}
+};
 
 Die.prototype.roll = function() {
-  this.pip = Math.floor((Math.random() * 6) + 1);
-}
+  return this.pip = Math.floor((Math.random() * 6) + 1);
+};
+
+function Player(){
+  this.score = 0;
+};
+
+function updateScore(player, points){
+  console.log(points);
+  player.score += points;
+  console.log(player.score);
+  return player.score;
+};
 
 $(document).ready(function(){
   die1 = new Die();
-  die1.roll();
-  console.log(die1.pip);
+  player1 = new Player();
+  player2 = new Player();
+  points = 0;
+
   $("#player1-roll").click(function(){
-    die1.roll();
-    $("#player1").text(die1.pip);
+    points = die1.roll();
+    console.log(points);
+    $("#player1").text(updateScore(player1, points));
   });
 
   $("#player2-roll").click(function(){
-    die1.roll();
-    $("#player2").text(die1.pip);
+    points = die1.roll();
+    console.log(points);
+    $("#player2").text(updateScore(player2, points));
   });
 
 });
