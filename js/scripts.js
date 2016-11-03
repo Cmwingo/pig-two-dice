38de887e45957617853d118482a22a4aa41ca299
Chris Wingo and Dave Denlow <dave@>
2016-11-03 10:36:38 -0700

    Now capable of playing the two dice variation of pig dice

--- js/scripts.js
+++ js/scripts.js
@@ -23,24 +23,44 @@ function updateScore(player, points){
 //user interface//
 $(document).ready(function(){
   die1 = new Die();
+  die2 = new Die();
   player1 = new Player(); //constructor call
   player2 = new Player();
-  score = 0;
+
+  function player1EndTurn() {
+    $("#round-total").text(roundPoints);
+    $("#player2-roll").prop("disabled", false);
+    $("#player1-roll").prop("disabled", true);
+    $("#player2-stop").prop("disabled", false);
+    $("#player1-stop").prop("disabled", true);
+    $("#player1-turn").hide();
+    $("#player2-turn").show();
+  };
+
+  function player2EndTurn() {
+    $("#player1-roll").prop("disabled", false);
+    $("#player2-roll").prop("disabled", true);
+    $("#player1-stop").prop("disabled", false);
+    $("#player2-stop").prop("disabled", true);
+    $("#player2-turn").hide();
+    $("#player1-turn").show();
+    $("#round-total").text(roundPoints);
+  }
 
   $("#player1-roll").click(function(){
     $("#current-roll").text("");
-    roll = die1.roll();
+    roll = die1.roll() + die2.roll();
+    console.log(roll);
     $("#round-total").text(roundPoints);
-    $("#current-roll").append("<img src=img/" + roll.toString() + ".png>");
-    if(roll === 1) {
+    $("#current-roll").append('<img src="img/' + die1.pip.toString() + '.png">' + '<img src="img/' + die2.pip.toString() + '.png">');
+    if(roll === 2){
       roundPoints = 0;
-      $("#round-total").text(roundPoints);
-      $("#player2-roll").prop("disabled", false);
-      $("#player1-roll").prop("disabled", true);
-      $("#player2-stop").prop("disabled", false);
-      $("#player1-stop").prop("disabled", true);
-      $("#player1-turn").hide();
-      $("#player2-turn").show();
+      player1.score = 0;
+      $("#player1").text("0");
+      player1EndTurn();
+    } else if(die1.pip === 1 || die2.pip === 1) {
+      roundPoints = 0;
+      player1EndTurn();
     } else {
       roundPoints += roll;
     }
@@ -49,16 +69,10 @@ $(document).ready(function(){
   });
 
   $("#player1-stop").click(function(){
-    $("#player2-roll").prop("disabled", false);
-    $("#player1-roll").prop("disabled", true);
-    $("#player2-stop").prop("disabled", false);
-    $("#player1-stop").prop("disabled", true);
-    $("#player1-turn").hide();
-    $("#player2-turn").show();
-    $("#round-total").text(roundPoints);
     $("#player1").text(updateScore(player1, roundPoints));
+      player1EndTurn();
     roundPoints = 0; //reset or value for new player//
-    if(player1.score >= 10){
+    if(player1.score >= 30){
       $("#player2-roll").prop("disabled", true);
       $("#player2-stop").prop("disabled", true);
       $("#winner").text('Player 1 Wins!');
@@ -69,18 +83,17 @@ $(document).ready(function(){
 
   $("#player2-roll").click(function(){
     $("#current-roll").text("");
-    roll = die1.roll();
+    roll = die1.roll() + die2.roll();
     $("#round-total").text(roundPoints);
-    $("#current-roll").append('<img src="img/' + roll.toString() + '.png">');
-    if(roll === 1) {
-      $("#round-total").text(roundPoints);
+    $("#current-roll").append('<img src="img/' + die1.pip.toString() + '.png">' + '<img src="img/' + die2.pip.toString() + '.png">');
+    if(roll === 2){
       roundPoints = 0;
-      $("#player1-roll").prop("disabled", false);
-      $("#player2-roll").prop("disabled", true);
-      $("#player1-stop").prop("disabled", false);
-      $("#player2-stop").prop("disabled", true);
-      $("#player2-turn").hide();
-      $("#player1-turn").show();
+      player2.score = 0;
+      $("#player2").text("0");
+      player2EndTurn();
+    } else if(die1.pip === 1 || die2.pip === 1) {
+      roundPoints = 0;
+      player2EndTurn();
     } else {
       roundPoints += roll;
     }
@@ -89,16 +102,10 @@ $(document).ready(function(){
   });
 
   $("#player2-stop").click(function(){
-    $("#player1-roll").prop("disabled", false);
-    $("#player2-roll").prop("disabled", true);
-    $("#player1-stop").prop("disabled", false);
-    $("#player2-stop").prop("disabled", true);
-    $("#player2-turn").hide();
-    $("#player1-turn").show();
-    $("#round-total").text(roundPoints);
     $("#player2").text(updateScore(player2, roundPoints));
+    player2EndTurn();
     roundpoints = 0;
-    if(player2.score >= 10){
+    if(player2.score >= 30){
       $("#player1-roll").prop("disabled", true);
       $("#player1-stop").prop("disabled", true);
       $("#winner").text('Player 2 Wins!');
